1be02234fa6281ff9f2dfed9545734224fee29c6
Khlein-dev <kierjerymiepascua@gmail.com>
2025-11-25 11:13:00 +0800

    data added

---
+++ Taskly/data/todo.js
@@ -0,0 +1,27 @@
+export const todos = [
+    {
+        id: 1,
+        title: "Buy groceries",
+        description: "Milk, Bread, Eggs, Butter",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "Finish project report",
+        description: "Due by end of the week",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "Do laundry",
+        description: "Wash and fold clothes",
+        completed: false,
+    },
+
+    {
+        id: 4,
+        title: "Walk the dog",
+        description: "30-minute walk in the park",
+        completed: false,
+    },
+];
